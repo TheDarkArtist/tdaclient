5f0392b51d9f8fb0a20ce7dd088c555305bf813f
Kushagra <sparrow.kushagra@gmail.com>
2024-05-27 18:14:48 +0530

    Added comments

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
+// Declare a global variable for PrismaClient to avoid multiple instances in development
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient({
-    log: ['error']
-});
+// Initialize PrismaClient and set logging level to 'error' to capture errors
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["error"],
+  });
 
+// In development, ensure PrismaClient instance is not recreated
 if (process.env.VERCEL_ENV === "development") global.prisma = prisma;
 
 export default prisma;
-
-
-
