276675de10651d3d662ca729b3c320bced029bd9
Kushagra <sparrow.kushagra@gmail.com>
2024-05-27 19:38:33 +0530

    Fixed sidebar

--- src/ui/sidenav/sidenav.tsx
+++ src/ui/sidenav/sidenav.tsx
@@ -31,7 +31,7 @@ const Sidenav = () => {
   return (
     <>
       <button
-        className="fixed z-30 right-2 top-3 md:hidden"
+        className="fixed z-40 right-2 top-3 md:hidden"
         onClick={toggleSidebar}
       >
         {showSidebar ? (
@@ -43,12 +43,12 @@ const Sidenav = () => {
 
       <div
         onClick={toggleSidebar}
-        className={`${showSidebar ? "w-full" : "w-0"} z-20 bg-transparent opacity-60 h-full fixed transition-all ease-in-out`}
+        className={`${showSidebar ? "w-full" : "w-0"} z-30 bg-black/[.6] dark:bg-grid-red-800/[.2] h-full fixed transition-all ease-in-out`}
       />
       <menu
         className={`transform right-0 ${
-          showSidebar ? "w-[80%] translate-x-0" : "translate-x-full"
-        } fixed z-20 max-w-80 flex h-full flex-col justify-between border-l dark:border-cyan-950 border-stone-600 transition-all ease-in-out bg-stone-400 text-white dark:bg-[#310101] md:hidden dark:backdrop-filter bg-opacity-80 backdrop-blur-sm dark:bg-opacity-40 dark:backdrop-blur-sm shadow-2xl dark:shadow-2xl shadow-stone-800 dark:shadow-red-600`}
+          showSidebar ? "w-[80%] translate-x-0" : "translate-x-96"
+        } fixed z-30 max-w-80 flex h-full flex-col justify-between border-l dark:bg-dot-red-800/[.4] dark:border-cyan-950 border-stone-600 transition-all ease-in-out bg-stone-400 text-white dark:bg-[#310101] md:hidden dark:backdrop-filter bg-opacity-80 backdrop-blur-sm dark:bg-opacity-40 dark:backdrop-blur-sm shadow-2xl dark:shadow-2xl shadow-stone-800 dark:shadow-red-600/[.6]`}
       >
         <SidenavCard toggleSidebar={toggleSidebar} />
         <SidenavLinks toggleSidebar={toggleSidebar} />
