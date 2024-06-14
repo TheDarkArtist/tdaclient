face38113c8779962c5cf199b8bdb5d0e3a0e008
Kushagra <sparrow.kushagra@gmail.com>
2024-06-14 19:12:57 +0530

    Fixed back button's ui

--- src/ui/utils/back.tsx
+++ src/ui/utils/back.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LuArrowLeft } from "react-icons/lu";
+import { LuChevronLeft } from "react-icons/lu";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -9,10 +9,10 @@ const GoBack = ({ href }: { href?: string }) => {
   return (
     <button
       onClick={() => (href ? router.push(href) : router.back())}
-      className="p-2 h-min space-x-2 flex border dark:border-stone-600 rounded-md dark:hover:border-stone-500"
+      className="p-2 h-min space-x-1 flex border dark:border-stone-600 rounded-md dark:hover:border-stone-500"
     >
-      <LuArrowLeft />
-      <span className="md:inline hidden">Back</span>
+      <LuChevronLeft className="h-5 w-5 my-auto" />
+      <span className="md:inline hidden my-auto">Back</span>
     </button>
   );
 };
