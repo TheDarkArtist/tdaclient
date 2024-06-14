fc96a696b3de439671f84c5102804e90d1c0b776
Kushagra <sparrow.kushagra@gmail.com>
2024-06-14 19:09:53 +0530

    Now works with dark and light mode seemlessly

--- src/ui/articles/article-tag.tsx
+++ src/ui/articles/article-tag.tsx
@@ -5,7 +5,7 @@ const ArticleTag = ({ tag }: { tag: string }) => {
     <>
       <span className="hover:animate-bounce relative inline-flex overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex 1-full text-xs w-full cursor-pointer items-center justify-center rounded-md dark:bg-slate-950 bg-gray-300 px-3 py-1 font-medium dark:text-white backdrop-blur-3xl">
+        <span className="inline-flex 1-full text-xs w-full cursor-pointer items-center justify-center rounded-md dark:bg-slate-950 bg-white px-3 py-1 font-medium dark:text-white backdrop-blur-3xl">
           {tag || "lang"}
         </span>
       </span>
