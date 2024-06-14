db794919bb46683aaaed51ee6f359da53b85c015
Kushagra <sparrow.kushagra@gmail.com>
2024-06-14 19:12:23 +0530

    Changed navbar to black for dark mode.

--- src/ui/nav/navbar.tsx
+++ src/ui/nav/navbar.tsx
@@ -7,7 +7,7 @@ import Sidenav from "../sidenav/sidenav";
 const Navbar = async () => {
   return (
     <>
-      <nav className="flex fixed z-30 w-full bg-white/[.6] dark:bg-gray-900/[.8] px-4 py-2 justify-between items-center dark:backdrop-filter dark:bg-opacity-60 dark:backdrop-blur-sm backdrop-filter  backdrop-blur-md">
+      <nav className="flex fixed z-30 w-full bg-white/[.6] dark:bg-black px-4 py-2 justify-between items-center dark:backdrop-filter dark:bg-opacity-60 dark:backdrop-blur-sm backdrop-filter  backdrop-blur-md">
         <section className="flex gap-10 items-center">
           <span className="w-20 h-min">
             <Image src="/logo.png" alt="TDA Logo" height={100} width={200} />
