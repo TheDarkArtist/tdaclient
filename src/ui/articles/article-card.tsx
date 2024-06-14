d13ac8f342e8e0b10d0e3c18e3ead3b6fe5b4fd5
Kushagra <sparrow.kushagra@gmail.com>
2024-06-14 19:10:42 +0530

    Now supports both dark and light theme.

--- src/ui/articles/article-card.tsx
+++ src/ui/articles/article-card.tsx
@@ -6,9 +6,9 @@ import ArticleTag from "./article-tag";
 export function ArticleCard({ data }: { data: Article }) {
   return (
     <div className="inter-va h-full relative">
-      <div className="bg-white relative cursor-pointer group/card  dark:hover:shadow-2xl dark:hover:shadow-cyan-500/[0.1] bg-grid-cyan-600/[.2] dark:bg-grid-cyan-600/[.2] dark:bg-black dark:border-white/[0.2] border-stone-600/[0.2] h-full w-full rounded-xl md:px-6 md:pt-10 p-3 border  hover:scale-[1.02] transition-all ease-in-out duration-200">
+      <div className="bg-white relative cursor-pointer group/card  dark:hover:shadow-2xl dark:hover:shadow-cyan-500/[0.1] bg-grid-sky-600/[.1] dark:bg-grid-green-600/[.1] dark:bg-black dark:border-sky-600/[0.4] border-sky-600/[0.4] h-full w-full rounded-xl md:px-6 md:pt-10 p-3 border  hover:scale-[1.02] transition-all ease-in-out duration-200">
         <div
-          className={`text-2xl font-bold text-neutral-600 w-full dark:text-sky-600`}
+          className={`text-2xl font-bold text-sky-600 w-full dark:text-sky-600`}
         >
           {data.title}
         </div>
