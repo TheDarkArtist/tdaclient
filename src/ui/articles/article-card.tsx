70f482dabbf9d39bb0ebe4b4e3b77ee14d54dbb6
Kushagra <sparrow.kushagra@gmail.com>
2024-06-16 11:31:53 +0530

    added views count to card

--- src/ui/articles/article-card.tsx
+++ src/ui/articles/article-card.tsx
@@ -2,11 +2,29 @@
 import React from "react";
 import { Article } from "@prisma/client";
 import ArticleTag from "./article-tag";
+import { LuEye } from "react-icons/lu";
+import { _incView } from "@/lib/actions/articles";
+import { getSession, useSession } from "next-auth/react";
 
 export function ArticleCard({ data }: { data: Article }) {
+  const session = useSession();
   return (
-    <div className="inter-va h-full relative">
+    <div
+      onClick={() => {
+        if (session.status !== "authenticated") {
+          _incView(data.id, "unknown");
+        } else _incView(data.id, session.data?.user.id as string);
+      }}
+      className="inter-va h-full relative"
+    >
       <div className="bg-white relative cursor-pointer group/card  dark:hover:shadow-2xl dark:hover:shadow-cyan-500/[0.1] bg-grid-sky-600/[.1] dark:bg-grid-green-600/[.1] dark:bg-black dark:border-sky-600/[0.4] border-sky-600/[0.4] h-full w-full rounded-xl md:px-6 md:pt-10 p-3 border  hover:scale-[1.02] transition-all ease-in-out duration-200">
+        <div className="flex justify-between items-center absolute top-3">
+          <div className="flex items-center space-x-1">
+            <LuEye className="h-5 w-5" />
+            <span>{data.views.length}</span>
+          </div>
+          <div></div>
+        </div>
         <div
           className={`text-2xl font-bold text-sky-600 w-full dark:text-sky-600`}
         >
