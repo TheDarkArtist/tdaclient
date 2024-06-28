ba709488971d88540f26b3b91c1953be55411e41
Kushagra <sparrow.kushagra@gmail.com>
2024-06-28 05:51:41 +0530

    New migrations added for Article Model

---
+++ src/prisma/migrations/20240626195717_new/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Article" ALTER COLUMN "tags" DROP DEFAULT;
---
+++ src/prisma/migrations/20240627125315_add_polymorphic_comments/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "parentId" TEXT NOT NULL,
+    "parentType" TEXT NOT NULL,
+    "articleId" TEXT,
+    "projectId" TEXT,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Comment_parentId_parentType_idx" ON "Comment"("parentId", "parentType");
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
