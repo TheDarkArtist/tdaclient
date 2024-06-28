7b6b3f00c3b87ffc9092b4412a223ab66538175b
Kushagra <sparrow.kushagra@gmail.com>
2024-06-28 06:11:40 +0530

    Fixed :)

--- src/ui/footer/footer.tsx
+++ src/ui/footer/footer.tsx
@@ -3,16 +3,7 @@ import { SparklesCore } from "../utils/sparkles";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center h-60 relative mt-10 dark:bg-dot-red-900">
-      <SparklesCore
-        background="transparent"
-        minSize={0.4}
-        maxSize={1}
-        particleDensity={200}
-        className="h-full w-full absolute bottom-0"
-        particleColor="#1793D1"
-      />
-    </div>
+      <div className="h-20"></div>
   );
 };
 
--- src/ui/home/latest-list.tsx
+++ src/ui/home/latest-list.tsx
@@ -19,33 +19,33 @@ const LatestList: React.FC<LatestListProps> = async ({
   const articles = await _latestArticles();
   const projects = await _latestProjects();
   return (
-      <BackgroundGradient>
-        <div className="dark:bg-black bg-white p-4 rounded-md">
-          <h1 className="text-2xl font-bold mb-2">{title}</h1>
-          <p className="mb-4">{description}</p>
-          <ul className="list-disc ml-6">
-            {(tda ? projects : articles).map((item) => (
-              <li className="hover:text-cyan-600" key={item.id}>
-                <Link
-                  href={`${tda ? "/projects/" : "/articles/"}` + item.id}
-                  key={item.id}
-                >
-                  {item.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <Link
-            className="flex items-center gap-2 text-cyan-800 group justify-end mt-4"
-            href={tda ? "/projects" : "/articles"}
-          >
-            <span className="hover:text-cyan-600 cursor-pointer">
-              All {tda ? "Projects" : "Articles"}
-            </span>
-            <LuMoveRight className="group-hover:text-cyan-600" />
-          </Link>
-        </div>
-      </BackgroundGradient>
+    <BackgroundGradient>
+      <div className="dark:bg-black bg-white p-4 rounded-md">
+        <h1 className="text-2xl font-bold mb-2">{title}</h1>
+        <p className="mb-4">{description}</p>
+        <ul className="list-disc ml-6">
+          {(tda ? projects : articles).map((item) => (
+            <li className="hover:text-cyan-600" key={item.id}>
+              <Link
+                href={`${tda ? "/projects/" : "/articles/"}` + item.id}
+                key={item.id}
+              >
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Link
+          className="flex items-center gap-2 text-cyan-800 group justify-end mt-4"
+          href={tda ? "/projects" : "/articles"}
+        >
+          <span className="hover:text-cyan-600 cursor-pointer">
+            All {tda ? "Projects" : "Articles"}
+          </span>
+          <LuMoveRight className="group-hover:text-cyan-600" />
+        </Link>
+      </div>
+    </BackgroundGradient>
   );
 };
 
