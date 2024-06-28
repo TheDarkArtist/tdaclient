938af577ee30817f04678f6549c071a3aa485fb3
Kushagra <sparrow.kushagra@gmail.com>
2024-06-28 05:35:47 +0530

    Added Draft, Published page for articles

---
+++ src/app/articles/drafts/page.tsx
@@ -0,0 +1,27 @@
+import { getSession } from "@/lib/auth";
+import DraftArticleList from "@/ui/articles/draft-article-list";
+import GoBack from "@/ui/utils/back";
+import { notFound } from "next/navigation";
+import React, { Suspense } from "react";
+
+const page = async () => {
+  const session = await getSession();
+  if (!session) notFound();
+  return (
+    <div className="flex justify-center">
+      <div className="mt-20 p-4 max-w-[60rem] w-full ">
+        <div className="flex justify-between mb-12">
+          <h1 className="text-2xl font-bold text-sky-600">
+            Your Draft Articles
+          </h1>
+          <GoBack href="/articles" />
+        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <DraftArticleList />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default page;
