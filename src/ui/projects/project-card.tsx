8b3997cd4b0ce7d0a306f26c6eb5cb7f54bc9f20
Kushagra <sparrow.kushagra@gmail.com>
2024-07-02 03:52:51 +0530

    Added timestamp, delete, edit, to project card

--- src/ui/projects/project-card.tsx
+++ src/ui/projects/project-card.tsx
@@ -4,13 +4,27 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/ui/utils/3d-card";
 import { Project } from "@prisma/client";
 import ProjectTag from "./project-tag";
-import { LuExternalLink, LuEye, LuGithub, LuLink } from "react-icons/lu";
+import {
+  LuExternalLink,
+  LuEye,
+  LuFileEdit,
+  LuGithub,
+  LuTrash,
+} from "react-icons/lu";
 import Link from "next/link";
-import { _incView } from "@/lib/actions/projects";
+import { _delete, _incView } from "@/lib/actions/projects";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import {
+  formatDistance,
+  formatDistanceToNow,
+  formatDistanceToNowStrict,
+} from "date-fns";
 
 export function ProjectCard({ data }: { data: Project }) {
   const session = useSession();
+  const router = useRouter();
+
   return (
     <CardContainer className="inter-va h-full" containerClassName="h-full">
       <CardBody className="bg-white relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-stone-600/[0.2] h-full w-full rounded-xl md:px-6 pt-10 pb-4 px-3 border">
@@ -18,12 +32,33 @@ export function ProjectCard({ data }: { data: Project }) {
           translateZ="50"
           className={`absolute flex justify-between w-full top-3 left-0 md:px-6 px-4 font-bold text-neutral-600 dark:text-sky-600`}
         >
-          <div className="flex items-center gap-1 text-sky-600">
-            <LuEye className="h-5 w-5 text-sky-600" />
-            <span>{data.views.length}</span>
+          <div className="flex gap-4">
+            <div className="flex items-center gap-1 text-sky-600">
+              <LuEye className="h-5 w-5 text-sky-600" />
+              <span>{data.views.length}</span>
+            </div>
+            <span>
+              {formatDistanceToNow(data.createdAt, { addSuffix: true })}
+            </span>
           </div>
           <div className="flex gap-2 items-center text-sky-600">
-            <Link href={data.link || ""}>
+            <LuTrash
+              className={`h-6 w-6 cursor-pointer hover:border border-sky-600 border-dotted ${session.data?.user.access === "root" ? "" : "hidden"}`}
+              onClick={async () => {
+                const confirm = window.confirm(
+                  "Are you sure, you want to delete it"
+                );
+                if (confirm) await _delete(data.id);
+                router.refresh();
+              }}
+            />
+            <Link
+              className={session.data?.user.access === "root" ? "" : "hidden"}
+              href={`/projects/post/${data.id}?source=up`}
+            >
+              <LuFileEdit className="h-6 w-6 cursor-pointer hover:border border-sky-600 border-dotted" />
+            </Link>
+            <Link href={data.repo || ""}>
               <LuGithub className="h-6 w-6 cursor-pointer hover:border border-sky-600 border-dotted" />
             </Link>
             <Link href={data.link || ""}>
