859a6afad3d854eb2ce40617aa4c6740ee32457e
Kushagra <sparrow.kushagra@gmail.com>
2024-07-04 01:07:42 +0530

    Refactored UI to be better

--- src/ui/projects/project-search.tsx
+++ src/ui/projects/project-search.tsx
@@ -22,7 +22,7 @@ export function ProjectSearch() {
     <div className="h-[4rem] flex justify-center w-full items-center">
       <input
         className="py-2.5 px-4 rounded-md dark:bg-zinc-950 border dark:border-stone-600/60 border-stone-300 w-full focus:outline-none"
-        placeholder="Search for articles"
+        placeholder="Search for projects"
         defaultValue={searchParams.get("query")?.toString() as string}
         onChange={(e) => {
           handleSearch(e.target.value);
--- src/ui/projects/project-tag.tsx
+++ src/ui/projects/project-tag.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const ProjectTag = ({ tag }: { tag: string }) => {
   return (
     <>
-      <span className="flex justify-center items-center hover:animate-bounce relative h-6 min-w-10 pt-[1px] pb-[1px] px-[1px] overflow-hidden rounded-full">
+      <span className="flex justify-center items-center hover:animate-bounce relative h-6 min-w-10 pt-[2px] pb-[1px] px-[1px] overflow-hidden rounded-full">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,var(--sky-500),var(--blue-900),var(--cyan-500))]" />
-        <span className="flex text-xs justify-center my-px items-center dark:bg-slate-950 bg-white w-full h-full px-3 z-20 pb-1 rounded-full">
+        <span className="flex text-xs justify-center my-px items-center dark:bg-slate-950 bg-white w-full h-full px-3 z-20 rounded-full">
           {tag || "lang"}
         </span>
       </span>
